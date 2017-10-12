8b3096522cb7c43d652f08402b0ee7196f470686
Terence Ponce <terenceponce@gmail.com>
2017-10-12 18:47:57 +0800

    Abstract account saga into its own file

---
+++ src/sagas/accounts.js
@@ -0,0 +1,33 @@
+import { call, put, takeEvery } from 'redux-saga/effects';
+
+import { api } from '../services';
+
+import {
+  FETCH_ACCOUNTS_REQUEST,
+  FETCH_ACCOUNTS_SUCCESS,
+  FETCH_ACCOUNTS_FAILURE,
+} from '../actions/accounts';
+
+function* fetchAccounts() {
+  try {
+    const apiCall = yield call(api.fetchAccounts);
+    const normalizedAccounts = Object.entries(apiCall.response.account).map((account) => {
+      return {
+        id: account[1].id,
+        name: account[1].attributes.name,
+        description: account[1].attributes.description,
+        category: account[1].attributes.category,
+      };
+    });
+
+    yield put({ type: FETCH_ACCOUNTS_SUCCESS, accounts: normalizedAccounts });
+  } catch (e) {
+    yield put({ type: FETCH_ACCOUNTS_FAILURE, message: e.message });
+  }
+}
+
+function* watchFetchAccounts() {
+  yield takeEvery(FETCH_ACCOUNTS_REQUEST, fetchAccounts);
+}
+
+export default watchFetchAccounts;
--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,36 +1,8 @@
 /* eslint arrow-body-style: 0 */
 
-import { call, put, takeEvery, all } from 'redux-saga/effects';
+import { all } from 'redux-saga/effects';
 
-import { api } from '../services';
-
-import {
-  FETCH_ACCOUNTS_REQUEST,
-  FETCH_ACCOUNTS_SUCCESS,
-  FETCH_ACCOUNTS_FAILURE,
-} from '../actions/accounts';
-
-function* fetchAccounts() {
-  try {
-    const apiCall = yield call(api.fetchAccounts);
-    const normalizedAccounts = Object.entries(apiCall.response.account).map((account) => {
-      return {
-        id: account[1].id,
-        name: account[1].attributes.name,
-        description: account[1].attributes.description,
-        category: account[1].attributes.category,
-      };
-    });
-
-    yield put({ type: FETCH_ACCOUNTS_SUCCESS, accounts: normalizedAccounts });
-  } catch (e) {
-    yield put({ type: FETCH_ACCOUNTS_FAILURE, message: e.message });
-  }
-}
-
-function* watchFetchAccounts() {
-  yield takeEvery(FETCH_ACCOUNTS_REQUEST, fetchAccounts);
-}
+import watchFetchAccounts from './accounts';
 
 export default function* rootSaga() {
   yield all([
