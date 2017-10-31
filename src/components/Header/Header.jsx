07e514eb2357d5731dedc641f74581caede162a2
Terence Ponce <terenceponce@gmail.com>
2017-10-31 23:20:12 +0800

    Convert Header component to Semantic UI

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,91 +1,73 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { withStyles } from 'material-ui/styles';
-import classNames from 'classnames';
 import { Link } from 'react-router-dom';
-import AppBar from 'material-ui/AppBar';
-import Toolbar from 'material-ui/Toolbar';
-import Typography from 'material-ui/Typography';
-import Button from 'material-ui/Button';
-import IconButton from 'material-ui/IconButton';
-import MenuIcon from 'material-ui-icons/Menu';
+import {
+  Container,
+  Menu,
+  Image,
+} from 'semantic-ui-react';
 
-import styles from './styles';
-import { openSideBar } from '../../actions/navigation';
 import { signOutRequest } from '../../actions/authentication';
 
-const SignInButton = () => (
-  <Button
-    color="contrast"
-    component={Link}
-    to="/sign_in"
-  >
-    Sign In
-  </Button>
+import logo from '../App/logo.png';
+
+const AuthenticatedMenu = ({ onSignOutClick }) => (
+  <Menu.Menu position="right">
+    <Menu.Item as={Link} to="/" onClick={onSignOutClick}>Sign Out</Menu.Item>
+  </Menu.Menu>
 );
 
-const SignOutButton = ({ onSignOutClick }) => (
-  <Button
-    color="contrast"
-    component={Link}
-    to="/"
-    onClick={onSignOutClick}
-  >
-    Sign Out
-  </Button>
-);
-
-SignOutButton.propTypes = {
+AuthenticatedMenu.propTypes = {
   onSignOutClick: PropTypes.func.isRequired,
 };
 
+const UnauthenticatedMenu = () => (
+  <Menu.Menu position="right">
+    <Menu.Item as={Link} to="/sign_in">Sign In</Menu.Item>
+    <Menu.Item as={Link} to="/sign_up">Sign Up</Menu.Item>
+  </Menu.Menu>
+);
+
 export const Header = ({
-  classes,
-  sideBarOpen,
-  onSideBarClick,
   isAuthenticated,
   onSignOutClick,
 }) => (
-  <AppBar className={classNames(classes.appBar, sideBarOpen && classes.appBarShift)}>
-    <Toolbar disableGutters={!sideBarOpen}>
-      <IconButton
-        color="contrast"
-        aria-label="open drawer"
-        className={classNames(classes.menuButton, sideBarOpen && classes.hide)}
-        onClick={onSideBarClick}
-      >
-        <MenuIcon />
-      </IconButton>
-      <Typography type="title" color="inherit" className={classes.flex} noWrap>
+  <Menu
+    fixed="top"
+    inverted
+  >
+    <Container>
+      <Menu.Item as={Link} to="/" header>
+        <Image
+          size="mini"
+          src={logo}
+          style={{ marginRight: '1.5em' }}
+        />
         Open Budget
-      </Typography>
+      </Menu.Item>
+      <Menu.Item as={Link} to="/">
+        Home
+      </Menu.Item>
       {isAuthenticated ?
-        <SignOutButton onSignOutClick={onSignOutClick} />
+        <AuthenticatedMenu onSignOutClick={onSignOutClick} />
         :
-        <SignInButton />
+        <UnauthenticatedMenu />
       }
-    </Toolbar>
-  </AppBar>
+    </Container>
+  </Menu>
 );
 
 Header.propTypes = {
-  classes: PropTypes.object.isRequired,
-  sideBarOpen: PropTypes.bool.isRequired,
-  onSideBarClick: PropTypes.func.isRequired,
   isAuthenticated: PropTypes.bool.isRequired,
   onSignOutClick: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = state => ({
-  sideBarOpen: state.navigation.sideBar.open,
   isAuthenticated: state.authentication.isAuthenticated,
 });
 
 const mapDispatchToProps = dispatch => ({
-  onSideBarClick: () => {
-    dispatch(openSideBar());
-  },
   onSignOutClick: () => {
     return new Promise((resolve) => {
       dispatch(signOutRequest(resolve));
@@ -93,6 +75,4 @@ const mapDispatchToProps = dispatch => ({
   },
 });
 
-const HeaderWithStyle = withStyles(styles)(Header);
-
-export default connect(mapStateToProps, mapDispatchToProps)(HeaderWithStyle);
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
