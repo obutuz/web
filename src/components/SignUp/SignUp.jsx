df39ce9d18ef79c0b125afee54b358de58f52c4f
Terence Ponce <terenceponce@gmail.com>
2017-11-06 14:38:20 +0800

    Redirect to home page after successful sign up

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -3,41 +3,53 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { reduxForm, Field } from 'redux-form';
 import { Button, Container, Form } from 'semantic-ui-react';
+import { Redirect } from 'react-router-dom';
 
 import { signUpRequest } from '../../actions/authentication';
 
 import TextField from '../Form/TextField';
 
-const SignUp = ({ handleSubmit, onSubmit }) => (
-  <Container>
-    <Form onSubmit={handleSubmit(onSubmit)}>
-      <Field
-        icon="user"
-        iconPosition="left"
-        id="email"
-        name="email"
-        placeholder="Email Address"
-        type="email"
-        component={TextField}
-      />
-      <Field
-        icon="lock"
-        iconPosition="left"
-        id="password"
-        name="password"
-        placeholder="Password"
-        type="password"
-        component={TextField}
-      />
-      <Button type="submit" color="teal">Submit</Button>
-    </Form>
-  </Container>
-);
+const SignUp = ({ handleSubmit, onSubmit, isAuthenticated }) => {
+  if (isAuthenticated) {
+    return <Redirect to="/" />;
+  }
+
+  return (
+    <Container>
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <Field
+          icon="user"
+          iconPosition="left"
+          id="email"
+          name="email"
+          placeholder="Email Address"
+          type="email"
+          component={TextField}
+        />
+        <Field
+          icon="lock"
+          iconPosition="left"
+          id="password"
+          name="password"
+          placeholder="Password"
+          type="password"
+          component={TextField}
+        />
+        <Button type="submit" color="teal">Submit</Button>
+      </Form>
+    </Container>
+  );
+}
 
 SignUp.propTypes = {
   handleSubmit: PropTypes.func.isRequired,
   onSubmit: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool.isRequired,
 };
+
+const mapStateToProps = state => ({
+  isAuthenticated: state.authentication.isAuthenticated,
+});
 
 const mapDispatchToProps = dispatch => ({
   onSubmit: (values) => {
@@ -49,4 +61,4 @@ const mapDispatchToProps = dispatch => ({
 
 const SignUpForm = reduxForm({ form: 'signUp' })(SignUp);
 
-export default connect(null, mapDispatchToProps)(SignUpForm);
+export default connect(mapStateToProps, mapDispatchToProps)(SignUpForm);
