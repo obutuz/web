2e35008c73f524a70c5088759b03736ad3945966
Terence Ponce <terenceponce@gmail.com>
2017-11-09 15:33:23 +0800

    Add TextArea and Select form field components

---
+++ src/components/Form/SelectField/SelectField.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Form } from 'semantic-ui-react';
+
+import BaseFormField from '../BaseFormField';
+
+const SelectField = ({
+  input,
+  id,
+  name,
+  placeholder,
+  options,
+}) => {
+  return (
+    <BaseFormField
+      id={id}
+      name={name}
+      placeholder={placeholder}
+      options={options}
+      input={input}
+      as={Form.Select}
+    />
+  );
+};
+
+SelectField.propTypes = {
+  input: PropTypes.object.isRequired,
+  id: PropTypes.string,
+  name: PropTypes.string,
+  placeholder: PropTypes.string,
+  options: PropTypes.arrayOf(PropTypes.shape({
+    key: PropTypes.string,
+    value: PropTypes.string.isRequired,
+    text: PropTypes.string.isRequired,
+  })).isRequired,
+};
+
+SelectField.defaultProps = {
+  id: '',
+  name: '',
+  placeholder: '',
+};
+
+export default SelectField;
+
