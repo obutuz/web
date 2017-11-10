66b4c28f5a36dd2c314e12cf5734075e01364749
Terence Ponce <terenceponce@gmail.com>
2017-11-10 20:48:10 +0800

    Add actions and reducers for Account Creation

--- src/actions/accounts.js
+++ src/actions/accounts.js
@@ -1,6 +1,9 @@
 export const FETCH_ACCOUNTS_REQUEST = 'FETCH_ACCOUNTS_REQUEST';
 export const FETCH_ACCOUNTS_SUCCESS = 'FETCH_ACCOUNTS_SUCCESS';
 export const FETCH_ACCOUNTS_FAILURE = 'FETCH_ACCOUNTS_FAILURE';
+export const CREATE_ACCOUNT_REQUEST = 'CREATE_ACCOUNT_REQUEST';
+export const CREATE_ACCOUNT_SUCCESS = 'CREATE_ACCOUNT_SUCCESS';
+export const CREATE_ACCOUNT_FAILURE = 'CREATE_ACCOUNT_FAILURE';
 
 export const fetchAccountsRequest = (resolve, reject) => ({
   type: FETCH_ACCOUNTS_REQUEST,
@@ -15,5 +18,22 @@ export const fetchAccountsSuccess = accounts => ({
 
 export const fetchAccountsFailure = error => ({
   type: FETCH_ACCOUNTS_FAILURE,
+  message: error.message,
+});
+
+export const createAccountRequest = (values, resolve, reject) => ({
+  type: CREATE_ACCOUNT_REQUEST,
+  values,
+  resolve,
+  reject,
+});
+
+export const createAccountSuccess = account => ({
+  type: CREATE_ACCOUNT_SUCCESS,
+  account,
+});
+
+export const createAccountFailure = error => ({
+  type: CREATE_ACCOUNT_FAILURE,
   message: error.message,
 });
