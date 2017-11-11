957b0c3c137eaad9ab2112e9fbb9ac1776fff546
Terence Ponce <terenceponce@gmail.com>
2017-11-11 09:10:04 +0800

    Improve account reducer state

--- src/reducers/accounts.js
+++ src/reducers/accounts.js
@@ -8,18 +8,11 @@ import {
 } from '../actions/accounts';
 
 const initialState = {
-  fetchAccounts: {
-    accounts: [],
-    isFetching: false,
-    success: false,
-    message: null,
-  },
-  createAccount: {
-    account: null,
-    isFetching: false,
-    success: false,
-    message: null,
-  },
+  accounts: [],
+  activeAccount: null,
+  isFetching: false,
+  success: false,
+  message: null,
 };
 
 const accounts = (state = initialState, { type, ...payload }) => {
@@ -27,62 +20,50 @@ const accounts = (state = initialState, { type, ...payload }) => {
     case FETCH_ACCOUNTS_REQUEST:
       return {
         ...state,
-        fetchAccounts: {
-          accounts: [],
-          isFetching: true,
-          success: false,
-          message: null,
-        },
+        accounts: [],
+        isFetching: true,
+        success: false,
+        message: null,
       };
     case FETCH_ACCOUNTS_SUCCESS:
       return {
         ...state,
-        fetchAccounts: {
-          accounts: payload.accounts,
-          isFetching: false,
-          success: true,
-          message: null,
-        },
+        accounts: payload.accounts,
+        isFetching: false,
+        success: true,
+        message: null,
       };
     case FETCH_ACCOUNTS_FAILURE:
       return {
         ...state,
-        fetchAccounts: {
-          accounts: [],
-          isFetching: false,
-          success: false,
-          message: payload.message,
-        },
+        accounts: [],
+        isFetching: false,
+        success: false,
+        message: payload.message,
       };
     case CREATE_ACCOUNT_REQUEST:
       return {
         ...state,
-        createAccount: {
-          account: null,
-          isFetching: true,
-          success: false,
-          message: null,
-        },
+        activeAccount: null,
+        isFetching: true,
+        success: false,
+        message: null,
       };
     case CREATE_ACCOUNT_SUCCESS:
       return {
         ...state,
-        createAccount: {
-          account: payload.account,
-          isFetching: false,
-          success: true,
-          message: null,
-        },
+        activeAccount: payload.account,
+        isFetching: false,
+        success: true,
+        message: null,
       };
     case CREATE_ACCOUNT_FAILURE:
       return {
         ...state,
-        createAccount: {
-          account: null,
-          isFetching: false,
-          success: false,
-          message: payload.message,
-        },
+        activeAccount: null,
+        isFetching: false,
+        success: false,
+        message: payload.message,
       };
     default:
       return state;
