69ec2788b07e84e2e0dcb36a782bc7bc46557879
Terence Ponce <terenceponce@gmail.com>
2017-11-11 09:55:43 +0800

    Connect BudgetsList component to redux

--- src/components/Budgets/Pages/List/List.jsx
+++ src/components/Budgets/Pages/List/List.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import { Container, Table } from 'semantic-ui-react';
+
+import { fetchBudgetsRequest } from '../../../../actions/budgets';
 
 export const List = ({ budgets }) => (
   <Container>
@@ -10,7 +13,6 @@ export const List = ({ budgets }) => (
           <Table.HeaderCell>ID</Table.HeaderCell>
           <Table.HeaderCell>Name</Table.HeaderCell>
           <Table.HeaderCell>Description</Table.HeaderCell>
-          <Table.HeaderCell>Category</Table.HeaderCell>
         </Table.Row>
       </Table.Header>
       <Table.Body>
@@ -36,4 +38,35 @@ List.propTypes = {
   }).isRequired).isRequired,
 };
 
-export default List;
+class BudgetsList extends React.Component {
+  componentDidMount() {
+    return new Promise((resolve, reject) => {
+      this.props.dispatch(fetchBudgetsRequest(resolve, reject));
+    });
+  }
+
+  render() {
+    return (
+      <List
+        budgets={this.props.budgets}
+      />
+    );
+  }
+}
+
+BudgetsList.propTypes = {
+  budgets: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    description: PropTypes.string,
+  }).isRequired).isRequired,
+  dispatch: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => {
+  return {
+    budgets: state.budgets.budgets,
+  };
+};
+
+export default connect(mapStateToProps, null)(BudgetsList);
