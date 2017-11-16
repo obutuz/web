2d3c7c53152b6254a711b07b1fb9fd9cec6a3837
Terence Ponce <terenceponce@gmail.com>
2017-11-16 09:46:33 +0800

    Set default budget on user sign in

--- src/sagas/authentication.js
+++ src/sagas/authentication.js
@@ -17,6 +17,8 @@ import {
   userAuthenticationCheckFailure,
 } from '../actions/authentication';
 
+import { switchBudgetSuccess } from '../actions/budgets';
+
 export function* signInUser() {
   while (true) {
     const { values, resolve, reject } = yield take(SIGN_IN_REQUEST);
@@ -24,8 +26,11 @@ export function* signInUser() {
     if (response && !error) {
       resolve();
       const authorizationHeader = response.headers.get('Authorization');
+      const defaultBudgetId = Object.entries(response.body.budget)[0][1].id;
       localStorage.setItem('authToken', authorizationHeader);
+      localStorage.setItem('defaultBudgetId', defaultBudgetId);
       yield put(signInSuccess(authorizationHeader));
+      yield put(switchBudgetSuccess(defaultBudgetId));
       yield put(push('/'));
     } else {
       reject();
